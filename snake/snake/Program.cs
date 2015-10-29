8a81460f9d99e12b53926607e39e884d83ad9e4e
alex_jd <twoalexan@mail.ru>
2015-10-29 17:25:28 +0500

    Компиляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -35,8 +35,7 @@ namespace snake
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+
 
             Console.ReadLine();
         }
